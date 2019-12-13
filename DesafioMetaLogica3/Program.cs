73fd893a1c93ba894a3603e9df4fc3b0909a2d51
insolito86 <delci_procopio@hotmail.com>
2019-12-13 00:54:34 -0300

    Implementação inicial

---
+++ DesafioMetaLogica3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] dias = new int[] { 7, 1, 5, 3, 6, 4 };
+        int diacompra = 0, diavenda = 0, lucro = 0;
+        Console.WriteLine(String.Format("Os valores das ações que serão avaliadas são {0}", String.Join(", ", dias)));
+        for (int x = 0; x < dias.Length; x++)
+        {
+            for (int y = x + 1; y < dias.Length; y++)
+            {
+                if (dias[y] - dias[x] > lucro)
+                {
+                    diacompra = x;
+                    diavenda = y;
+                    lucro = dias[diavenda] - dias[diacompra];
+                }
+            }
+        }
+        if (lucro > 0) Console.WriteLine(String.Format("Melhor lucro é de {0}, comprando no dia {1} e vendendo no dia {2} ({3} - {4} = {0})", lucro, diacompra + 1, diavenda + 1, dias[diavenda], dias[diacompra]));
+        else Console.WriteLine("Nenhuma transação deve ser feita, lucro máximo igual a 0");
+        Console.ReadKey();
+    }
+}
